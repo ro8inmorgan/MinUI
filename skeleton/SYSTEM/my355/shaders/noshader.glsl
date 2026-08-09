375b907f55048c4f06773127cdbeea9a87d2172c
Aurélien Pommel <aurelien.pommel@gmail.com>
2026-08-09 17:29:58 +0200

    my355: review all emulator cores and shaders

--- skeleton/SYSTEM/my355/shaders/noshader.glsl
+++ skeleton/SYSTEM/my355/shaders/noshader.glsl
@@ -5,7 +5,7 @@ varying vec2 vTexCoord;
 
 void main() {
     // Flip Y-axis in the vertex shader
-    vTexCoord = vec2(TexCoord.x, 1.0 - TexCoord.y);  // Flip Y
+    vTexCoord = vec2(TexCoord.x, TexCoord.y);  // Flip Y
     gl_Position = vec4(VertexCoord, 0.0, 1.0);
 }
 #endif
