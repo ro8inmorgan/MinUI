b1a2619c755ece7067fc8058c8c6273d08256b59
Aurélien Pommel <aurelien.pommel@gmail.com>
2026-08-09 18:08:29 +0200

    fix: my355 WiFi and Bluetooth stop and start on suspend

--- skeleton/SYSTEM/my355/etc/wifi/wifi_init.sh
+++ skeleton/SYSTEM/my355/etc/wifi/wifi_init.sh
@@ -3,10 +3,26 @@
 WIFI_INTERFACE="wlan0"
 WPA_SUPPLICANT_CONF="/userdata/cfg/wpa_supplicant.conf"
 
+# The USB wifi adapter is re-enumerated on resume, so wlan0 is gone for a couple
+# of seconds. Without waiting, wpa_supplicant binds to a missing interface.
+wait_for_interface() {
+	i=0
+	while [ ! -e "/sys/class/net/$WIFI_INTERFACE" ]; do
+		i=$((i + 1))
+		if [ $i -gt 50 ]; then # ~10s
+			echo "wifi_init: $WIFI_INTERFACE did not appear, continuing anyway" >&2
+			return 1
+		fi
+		sleep 0.2
+	done
+	return 0
+}
+
 start() {
 	insmod /system/lib/modules/RTL8189FU.ko 2>/dev/null
 	rfkill.elf unblock wifi
 	/etc/init.d/S36load_wifi_modules start
+	wait_for_interface
 	/etc/init.d/S40network start
 	/etc/init.d/S41dhcpcd start
 
