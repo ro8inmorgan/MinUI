feed9d9cd12c31511a23e94314a4e036f6ce0adc
Aurélien Pommel <aurelien.pommel@gmail.com>
2026-08-22 19:32:01 +0200

    revert: improve schedutil performance on multithreaded cores
    This reverts commit bed0b0450eb1b8342f3015f25abfe092e18ed42b.
    This created a performance regression that I thought I measured properly. In the end it seems to be rather a SotN specific performance issue.

--- skeleton/SYSTEM/my355/bin/governor.sh
+++ skeleton/SYSTEM/my355/bin/governor.sh
@@ -37,20 +37,10 @@ set_policy() {
         echo "$MAX_FREQ" > "$policy_path/scaling_max_freq"
 }
 
-# schedutil's defaults leave the CPU at 408-600 MHz under emulation
-tune_schedutil() {
-        local policy_path="$1"
-
-        [ -d "$policy_path/schedutil" ] || return 0
-        echo 45 > "$policy_path/schedutil/target_load" 2>/dev/null || true
-        echo 1000 > "$policy_path/schedutil/rate_limit_us" 2>/dev/null || true
-}
-
 case "$MODE" in
         auto)
                 # schedutil governor, min freq to one step below max (408-1800 MHz on MY355)
                 set_policy /sys/devices/system/cpu/cpufreq/policy0 "schedutil" "second_max"
-                tune_schedutil /sys/devices/system/cpu/cpufreq/policy0
                 ;;
         performance)
                 # performance governor, max freq (1992 MHz on MY355)
