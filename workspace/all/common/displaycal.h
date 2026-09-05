5501d1f9db564f3279360fb790bfd9e842a23859
Prashant Vaibhav <prashant@vaibhav.de>
2026-09-05 04:01:55 +0200

    refactor(displaycal): use direct values for all presets
    Remove per-device gain macros and the H700 uncalibrated initializer macro. Handle Brick Pro in the preset switch while preserving all calibration values.
    
    Review: https://github.com/LoveRetro/NextUI/pull/807#discussion_r3807740206

--- workspace/all/common/displaycal.h
+++ workspace/all/common/displaycal.h
@@ -9,21 +9,6 @@
 #define DISPLAYCAL_DEFAULT_GREEN_GAIN 100
 #define DISPLAYCAL_DEFAULT_BLUE_GAIN 100
 
-#define DISPLAYCAL_BRICK_DEFAULT_ENABLED 1
-#define DISPLAYCAL_BRICK_DEFAULT_RED_GAIN DISPLAYCAL_DEFAULT_RED_GAIN
-#define DISPLAYCAL_BRICK_DEFAULT_GREEN_GAIN 92
-#define DISPLAYCAL_BRICK_DEFAULT_BLUE_GAIN 58
-
-#define DISPLAYCAL_SMARTPRO_DEFAULT_ENABLED 1
-#define DISPLAYCAL_SMARTPRO_DEFAULT_RED_GAIN DISPLAYCAL_DEFAULT_RED_GAIN
-#define DISPLAYCAL_SMARTPRO_DEFAULT_GREEN_GAIN 77
-#define DISPLAYCAL_SMARTPRO_DEFAULT_BLUE_GAIN 50
-
-#define DISPLAYCAL_BRICKPRO_DEFAULT_ENABLED 1
-#define DISPLAYCAL_BRICKPRO_DEFAULT_RED_GAIN DISPLAYCAL_DEFAULT_RED_GAIN
-#define DISPLAYCAL_BRICKPRO_DEFAULT_GREEN_GAIN 94
-#define DISPLAYCAL_BRICKPRO_DEFAULT_BLUE_GAIN 54
-
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -36,42 +21,26 @@ typedef struct DisplayCalDefaults {
 } DisplayCalDefaults;
 
 // TrimUI Brick, measured with X-Rite i1Display Pro, calibrated to sRGB D65 2.2
-static const struct DisplayCalDefaults DisplayCalDefaults_Brick = {
-	DISPLAYCAL_BRICK_DEFAULT_ENABLED,
-	DISPLAYCAL_BRICK_DEFAULT_RED_GAIN,
-	DISPLAYCAL_BRICK_DEFAULT_GREEN_GAIN,
-	DISPLAYCAL_BRICK_DEFAULT_BLUE_GAIN
-};
+static const struct DisplayCalDefaults DisplayCalDefaults_Brick = { 1, 100, 92, 58 };
 
 // TrimUI Smart Pro, measured with Spyder 5 Pro and slightly modified by eye to match the Brick
-static const struct DisplayCalDefaults DisplayCalDefaults_SmartPro = {
-	DISPLAYCAL_SMARTPRO_DEFAULT_ENABLED,
-	DISPLAYCAL_SMARTPRO_DEFAULT_RED_GAIN,
-	DISPLAYCAL_SMARTPRO_DEFAULT_GREEN_GAIN,
-	DISPLAYCAL_SMARTPRO_DEFAULT_BLUE_GAIN
-};
+static const struct DisplayCalDefaults DisplayCalDefaults_SmartPro = { 1, 100, 77, 50 };
 
 // TrimUI Brick Pro, TODO
-static const struct DisplayCalDefaults DisplayCalDefaults_BrickPro = {
-	DISPLAYCAL_BRICKPRO_DEFAULT_ENABLED,
-	DISPLAYCAL_BRICKPRO_DEFAULT_RED_GAIN,
-	DISPLAYCAL_BRICKPRO_DEFAULT_GREEN_GAIN,
-	DISPLAYCAL_BRICKPRO_DEFAULT_BLUE_GAIN
-};
+static const struct DisplayCalDefaults DisplayCalDefaults_BrickPro = { 1, 100, 94, 54 };
 
 // Anbernic H700 family. Uncalibrated models remain disabled with neutral gains.
-#define DISPLAYCAL_H700_UNCALIBRATED { 0, 100, 100, 100 }
 static const struct DisplayCalDefaults DisplayCalDefaults_RG28XX = { 1, 100, 92, 65 };
-static const struct DisplayCalDefaults DisplayCalDefaults_RG34XX = DISPLAYCAL_H700_UNCALIBRATED;
+static const struct DisplayCalDefaults DisplayCalDefaults_RG34XX = { 0, 100, 100, 100 };
 static const struct DisplayCalDefaults DisplayCalDefaults_RG34XXSP = { 1, 100, 83, 86 };
 static const struct DisplayCalDefaults DisplayCalDefaults_RGSP = { 1, 100, 68, 61 };
-static const struct DisplayCalDefaults DisplayCalDefaults_RG35XXPlus = DISPLAYCAL_H700_UNCALIBRATED;
-static const struct DisplayCalDefaults DisplayCalDefaults_RG35XXH = DISPLAYCAL_H700_UNCALIBRATED;
-static const struct DisplayCalDefaults DisplayCalDefaults_RG35XXSP = DISPLAYCAL_H700_UNCALIBRATED;
-static const struct DisplayCalDefaults DisplayCalDefaults_RG35XXPRO = DISPLAYCAL_H700_UNCALIBRATED;
-static const struct DisplayCalDefaults DisplayCalDefaults_RG40XXH = DISPLAYCAL_H700_UNCALIBRATED;
+static const struct DisplayCalDefaults DisplayCalDefaults_RG35XXPlus = { 0, 100, 100, 100 };
+static const struct DisplayCalDefaults DisplayCalDefaults_RG35XXH = { 0, 100, 100, 100 };
+static const struct DisplayCalDefaults DisplayCalDefaults_RG35XXSP = { 0, 100, 100, 100 };
+static const struct DisplayCalDefaults DisplayCalDefaults_RG35XXPRO = { 0, 100, 100, 100 };
+static const struct DisplayCalDefaults DisplayCalDefaults_RG40XXH = { 0, 100, 100, 100 };
 static const struct DisplayCalDefaults DisplayCalDefaults_RG40XXV = { 1, 91, 100, 63 };
-static const struct DisplayCalDefaults DisplayCalDefaults_RGCubeXX = DISPLAYCAL_H700_UNCALIBRATED;
+static const struct DisplayCalDefaults DisplayCalDefaults_RGCubeXX = { 0, 100, 100, 100 };
 
 enum DisplayCalPreset {
 	DISPLAYCAL_PRESET_DEFAULT = 0,
@@ -95,6 +64,7 @@ static inline DisplayCalDefaults DisplayCal_getDefaultSettings(enum DisplayCalPr
 	switch(preset) {
 		case DISPLAYCAL_PRESET_SMARTPRO: return DisplayCalDefaults_SmartPro;
 		case DISPLAYCAL_PRESET_BRICK: return DisplayCalDefaults_Brick;
+		case DISPLAYCAL_PRESET_BRICKPRO: return DisplayCalDefaults_BrickPro;
 		case DISPLAYCAL_PRESET_RG28XX: return DisplayCalDefaults_RG28XX;
 		case DISPLAYCAL_PRESET_RG34XX: return DisplayCalDefaults_RG34XX;
 		case DISPLAYCAL_PRESET_RG34XXSP: return DisplayCalDefaults_RG34XXSP;
@@ -107,9 +77,6 @@ static inline DisplayCalDefaults DisplayCal_getDefaultSettings(enum DisplayCalPr
 		case DISPLAYCAL_PRESET_RG40XXV: return DisplayCalDefaults_RG40XXV;
 		case DISPLAYCAL_PRESET_RGCUBEXX: return DisplayCalDefaults_RGCubeXX;
 		default: break;
-	}
-	if(preset == DISPLAYCAL_PRESET_BRICKPRO) {
-		return DisplayCalDefaults_BrickPro;
 	}
 	// Default preset
 	DisplayCalDefaults defaults = {
